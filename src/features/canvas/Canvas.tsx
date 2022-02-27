d256e4fdd22be686ceb3fae0bf8abf56af4c6372
fiote <murilo.eng@gmail.com>
2022-02-27 01:46:20 -0300

    magic wand core

--- src/features/canvas/Canvas.tsx
+++ src/features/canvas/Canvas.tsx
@@ -5,7 +5,53 @@ import { ColorCounter } from './controlsSlice';
 export const refCanvas = createRef<HTMLCanvasElement>();
 const refFeature = createRef<HTMLDivElement>();
 
-interface Point {
+const Canvas = () => {
+
+	useLayoutEffect(() => {
+		window.addEventListener('resize',applyResizeToCanvas);
+		applyResizeToCanvas();
+	},[]);
+
+	const onMouseDown = (event: React.MouseEvent) => {
+		const { offsetX: x, offsetY: y } = event.nativeEvent;
+		listeners.onMouseDown?.call(null, x, y);
+	};
+
+	const onMouseUp = (event: React.MouseEvent) => {
+		const { offsetX: x, offsetY: y } = event.nativeEvent;
+		listeners.onMouseUp?.call(null, x, y);
+	};
+
+	return (
+		<div id="feature-canvas" ref={refFeature}>
+			<canvas data-testid="main-canvas" className="empty" ref={refCanvas} onMouseDown={onMouseDown} onMouseUp={onMouseUp} />
+		</div>
+	)
+};
+
+export default Canvas;
+
+// ===== LISTENERS ==================================================
+
+type IMousePoint = (x: number, y: number) => void;
+
+const listeners = {
+} as {
+	[key: string]: IMousePoint;
+}
+
+export const setCanvasListener = (key: string, fn: IMousePoint) => {
+	listeners[key] = fn;
+};
+
+export const removeCanvasListeners = () => {
+	const keys = Object.keys(listeners);
+	keys.forEach(key => delete listeners[key]);
+};
+
+// ===== FEATURE VARIABLES ==========================================
+
+export interface Point {
 	id: number;
 	block: number;
 	x: number;
@@ -14,46 +60,15 @@ interface Point {
 	color: number;
 }
 
-type v3 = [number, number, number];
+export type v3 = [number, number, number];
 
-const grid = [] as Point[];
+export const canvasGrid = [] as Point[];
 
 const bounds = {
 	min: {x: 0, y: 0},
-	max: {x:Number.POSITIVE_INFINITY, y: Number.POSITIVE_INFINITY}
+	max: {x:Number.POSITIVE_INFINITY, y: Number.POSITIVE_INFINITY},
+	size: {width: 0, height: 0}
 };
-
-const Canvas = () => {
-
-	useLayoutEffect(() => {
-		window.addEventListener('resize',applyResizeToCanvas);
-		applyResizeToCanvas();
-	},[]);
-
-	const onClick = () => {
-
-	};
-
-	const onMouseDown = () => {
-
-	};
-
-	const onMouseUp = () => {
-
-	};
-
-	return (
-		<div id="feature-canvas" ref={refFeature}>
-			<canvas data-testid="main-canvas" className="empty" ref={refCanvas} onClick={onClick} onMouseDown={onMouseDown} onMouseUp={onMouseUp} />
-		</div>
-	)
-};
-
-// ===== EVENT FUNCTIONS ============================================
-
-export const listeners = {
-
-}
 
 // ===== CANVAS FUNCTIONS ===========================================
 
@@ -97,8 +112,8 @@ export const setImageToCanvas = (image: HTMLImageElement) : {colors?: ColorCount
 		iHeight *= prMin;
 	}
 
-	canvas.width = cWidth;
-	canvas.height = cHeight;
+	bounds.size.width = canvas.width = cWidth;
+	bounds.size.height = canvas.height = cHeight;
 
 	let sx = Math.floor((cWidth - iWidth)/2);
 	let sy = Math.floor((cHeight - iHeight)/2);
@@ -118,8 +133,8 @@ export const setImageToCanvas = (image: HTMLImageElement) : {colors?: ColorCount
 	return { colors };
 };
 
-const readCanvasGrid = () => {
-	grid.length = 0;
+export const readCanvasGrid = () => {
+	canvasGrid.length = 0;
 
 	const canvas = refCanvas.current;
 	if (!canvas) return;
@@ -127,28 +142,62 @@ const readCanvasGrid = () => {
 	const ctx = canvas.getContext("2d");
 	if (!ctx) return;
 
-	let id = 0;
 	const { min, max } = bounds;
 
 	for (let x = min.x; x < max.x; x++) {
 		for (let y = min.y; y < max.y; y++) {
+			const id = getPointId(x, y);
 			const [r,g,b] = ctx.getImageData(x, y, 1, 1).data;
 			const rgb = [r,g,b] as v3;
 			const color = getIntFromRGB(rgb);
-			grid.push({id, block: 0, x, y, rgb, color});
-			id++;
+			canvasGrid[id] = {id, block: 0, x, y, rgb, color};
 		}
 	}
 };
 
+export const getCanvasPoint = (x: number, y: number) => {
+	const id = getPointId(x, y);
+	return canvasGrid[id];
+}
+
 const getCanvasColors = () => {
 	const colors = {} as {[key: number]: number};
-	grid.forEach(p => {
+	canvasGrid.forEach(p => {
 		if (!colors[p.color]) colors[p.color] = 0;
 		colors[p.color]++;
 	});
 	return colors;
 };
+
+export const getPointId = (x: number, y: number) => {
+	if (x < 0 || y < 0 || x >= bounds.size.width || y >= bounds.size.height) return -1;
+	return x*bounds.size.height + y;
+}
+
+// ===== BRUSH FUNCTIONS ============================================
+
+let brush : ImageData;
+let brushData : Uint8ClampedArray;
+
+export const paintPixel = (x: number, y: number, rgb: v3) => {
+	const canvas = refCanvas.current;
+	const ctx = canvas?.getContext("2d");
+	if (!ctx) return;
+
+	if (!brush) {
+		brush = ctx.createImageData(1,1);
+		brushData = brush.data;
+	}
+
+	const [r,g,b] = rgb;
+	brushData[0] = r;
+	brushData[1] = g;
+	brushData[2] = b;
+	brushData[3] = 255;
+	ctx.putImageData(brush, x, y);
+}
+
+// ===== IMAGE FUNCTIONS ============================================
 
 export const getFileAsBase64 = (file: File): Promise<string | undefined> => {
 	return new Promise(resolve => {
@@ -195,5 +244,3 @@ export const getHexFromRGB = (rgb: v3) => {
   	const hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 	return hex.toUpperCase();
 }
-
-export default Canvas;
--- src/features/magicwand/MagicWand.tsx
+++ src/features/magicwand/MagicWand.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { refCanvas } from '../canvas/Canvas';
+import { canvasGrid, getCanvasPoint, getPointId, paintPixel, Point, readCanvasGrid, refCanvas, setCanvasListener, v3 } from '../canvas/Canvas';
 import MenuButton from '../menu/MenuButton';
 
 const MagicWand = () => {
 
 	const onClick = () => {
 		const canvas = refCanvas.current;
+
+		setCanvasListener('onMouseUp', (x, y) => {
+			execMagicWandAt(x,y);
+		});
+
 		if (!canvas) return;
 	};
 
@@ -17,3 +22,179 @@ const MagicWand = () => {
 }
 
 export default MagicWand;
+
+
+// ===== MAGIC WAND =================================================
+
+let paintedBorder = [] as number[];
+let selectedArea = [] as number[];
+
+const execMagicWandAt = (x: number, y: number) => {
+	// 'unpaiting' any border already painted
+	paintedBorder.forEach(id => paintPoint(id));
+	// resetting the border data
+	paintedBorder = [];
+	// if the grid is not mapped, read it
+	if (!canvasGrid?.length) readCanvasGrid();
+	// get the clicked point based on its xy
+	const point = getCanvasPoint(x,y);
+	// if it does not exist, stop here
+	if (!point) return;
+	// if we're clicking inside of the selected area, stop here
+	if (selectedArea.includes(point.id)) {
+		selectedArea = [];
+		return;
+	}
+	// getting the area around the point
+	const {area, border} = getAreaAroundPoint(point);
+	// painting its border
+	border.forEach(id => paintBorder(id));
+	// storing the data
+	paintedBorder = border;
+	selectedArea = area;
+}
+
+const adders = [[0,-1], [0,+1], [-1,0], [+1,0]];
+let nextBlockId = 0;
+
+// flags the whole canvasGrid based on the clicked pixel and the selected tolerance, the get the affected area and its border
+const getAreaAroundPoint = (clicked: Point, extra: boolean = false) => {
+	console.log('getAreaAroundPoint()');
+	console.log(clicked);
+
+	// create a map to mark every pixel with the answer "does this color is within tolerance-distance of thec clicked point?""
+	const map = [] as number[][];
+
+	const tolerance = 1;
+
+	// for every pixel
+	for(const p1 of canvasGrid) {
+		if (!p1) continue;
+		// we get its position
+		const { x, y } = p1;
+		// and the delta color between the clicked pixel and it
+		const delta = getColorDelta(clicked, p1);
+		// if there is no map for its line yet, create one
+		if (!map[y]) map[y] = [];
+		// then map its pixel flag
+		map[y][x] = delta <= tolerance ? 1 : 0;
+	}
+	// increasing the blocker count	and setting it for the clicked pixel
+	clicked.block = ++nextBlockId;
+	// creating a control stack so we can keep expanding and processing pixels
+	const stack = [clicked.id];
+	// while also storing all the pixels inside its area
+	const area = [clicked.id];
+	// and the 'border' pixels so we can atually show the selection
+	const border = [] as number[];
+	// while there are pixels to process
+	while (stack.length) {
+		// get the first on the list
+		const id1 = stack.shift();
+		if (!id1) continue;
+		// get the canvasGrid entry for it
+		const p1 = canvasGrid[id1];
+		// get its coords
+		const {x,y} = p1;
+		// run the adders to iterate around the pixel
+		for(const add of adders) {
+			// get the neighbor coords
+			const x2 = x+add[0];
+			const y2 = y+add[1];
+			// get its id
+			const id2 = getPointId(x2,y2);
+			const p2 = canvasGrid[id2];
+			// if there is no such id (this means it's outside of the canvas), we skip the add
+			if (!p2) continue;
+			// otherwise, we get the flag for it
+			const flag = map[y2][x2];
+			if (flag > 0)  {
+				// if it's marked as tolerated, let's unset it so it won't be processed again
+				map[y2][x2] = -1;
+				// then we add it to the stack, so we can process around it as well
+				stack.push(id2);
+				area.push(id2);
+				// marking the neighbor with the same block value
+				canvasGrid[id2].block = clicked.block;
+			}
+			if (flag === 0) {
+				border.push(id2);
+				map[y2][x2] = -1;
+			}
+		}
+	}
+
+	return { area, border };
+}
+
+// ===== BORDER CONTROL =============================================
+
+// paints a red pixel based on a pixel xy position
+const paintBorder = (id: number) => {
+	const p = canvasGrid[id];
+	if (p) 	paintPixel(p.x, p.y, [255,0,0]);
+}
+
+// paints the actual pixel back to its xy position
+const paintPoint = (id: number) => {
+	const p = canvasGrid[id];
+	paintPixel(p.x, p.y, p.rgb);
+}
+
+// ===== COLOR DELTA ================================================
+
+// chache object so we can store delta values between colors, so we don't need to calculate it again
+let colorCache = {} as {[key: string]: number};
+
+// returns the delta difference between two points on the canvas
+const getColorDelta = (p1: Point, p2: Point) => {
+	// if their color is the same, the delta is of course zero
+	if (p1.color === p2.color) return 0;
+	// creating a mash string so we can check its cache
+	const mash = p1.color+'.'+p2.color;
+	const cached = colorCache[mash];
+	// if there is a cache, return it
+	if (cached !== undefined) return cached;
+	// if there is not, actually calculate the differnce
+	const delta = deltaE(p1.rgb, p2.rgb);
+	// cache it
+	colorCache[mash] = delta;
+	// then return!
+	return delta;
+}
+
+// rgb color comparation function (from: https://github.com/antimatter15/rgb-lab)
+
+function deltaE(rgbA: v3, rgbB: v3) {
+	let labA = rgb2lab(rgbA);
+	let labB = rgb2lab(rgbB);
+	let deltaL = labA[0] - labB[0];
+	let deltaA = labA[1] - labB[1];
+	let deltaB = labA[2] - labB[2];
+	let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+	let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+	let deltaC = c1 - c2;
+	let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+	deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+	let sc = 1.0 + 0.045 * c1;
+	let sh = 1.0 + 0.015 * c1;
+	let deltaLKlsl = deltaL / (1.0);
+	let deltaCkcsc = deltaC / (sc);
+	let deltaHkhsh = deltaH / (sh);
+	let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+	return i < 0 ? 0 : Math.sqrt(i);
+}
+
+function rgb2lab(rgb: v3) {
+	let r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255, x, y, z;
+	r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+	g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+	b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+	x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+	y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+	z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+	x = (x > 0.008856) ? Math.pow(x, 1 / 3) : (7.787 * x) + 16 / 116;
+	y = (y > 0.008856) ? Math.pow(y, 1 / 3) : (7.787 * y) + 16 / 116;
+	z = (z > 0.008856) ? Math.pow(z, 1 / 3) : (7.787 * z) + 16 / 116;
+	return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+}
