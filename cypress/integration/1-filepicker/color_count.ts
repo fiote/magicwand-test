68479168aee7483dfa1d359816983672fb10d429
fiote <murilo.eng@gmail.com>
2022-02-27 20:00:20 -0300

    fixing tests after transparent-color remove changes

--- cypress/integration/1-filepicker/color_count.ts
+++ cypress/integration/1-filepicker/color_count.ts
@@ -3,14 +3,14 @@ import { checkTotal, openImage } from "../../support/functions";
 describe('Check total of found colors', () => {
 	it('image#1', () => {
 		openImage('image-1.png');
-		checkTotal('pixels', 250);
+		checkTotal('pixels', 249);
 	});
 	it('image#2', () => {
 		openImage('image-2.png');
-		checkTotal('pixels', 149);
+		checkTotal('pixels', 148);
 	});
 	it('image#3', () => {
 		openImage('image-3.png');
-		checkTotal('pixels', 238);
+		checkTotal('pixels', 237);
 	});
 });
