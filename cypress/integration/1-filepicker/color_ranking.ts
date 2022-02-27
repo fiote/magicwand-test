2f79d6dbbf102581e5b4906aa7d5a5ea2416c96f
fiote <murilo.eng@gmail.com>
2022-02-27 20:23:42 -0300

    updating ranking tests after the remove of fully transparent counts

--- cypress/integration/1-filepicker/color_ranking.ts
+++ cypress/integration/1-filepicker/color_ranking.ts
@@ -3,17 +3,17 @@ import { checkRow, openImage } from "../../support/functions";
 describe('Check of specific colors are found on a specific order', () => {
 	it('image#1', () => {
 		openImage('image-1.png');
-		checkRow('pixels', 3, '#434343', 599);
-		checkRow('pixels', 16, '#FE5050', 144);
+		checkRow('pixels', 2, '#434343', 599);
+		checkRow('pixels', 15, '#FE5050', 144);
 	});
 	it('image#2', () => {
 		openImage('image-2.png');
-		checkRow('pixels', 1, '#262626', 7026);
-		checkRow('pixels', 11, '#ED42F0', 240);
+		checkRow('pixels', 0, '#262626', 7026);
+		checkRow('pixels', 10, '#ED42F0', 240);
 	});
 	it('image#3', () => {
 		openImage('image-3.png');
-		checkRow('pixels', 7, '#F1F1F1', 353);
-		checkRow('pixels', 16, '#F9D848', 154);
+		checkRow('pixels', 6, '#F1F1F1', 353);
+		checkRow('pixels', 15, '#F9D848', 154);
 	});
 });
